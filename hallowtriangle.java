c5acdea14ae36d16449551e21af90408aeb88dfc
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-08 12:42:56 +0530

    Add files via upload

---
+++ hallowtriangle.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class hallowtriangle {
+    public static void printPattern(int n)
+    {
+        int i, j, k;
+        for (i = 1; i <= n; i++) {
+    for (j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+          
+            for (k = 1; k <= (2 * i - 1); k++) {
+                if (k == 1 || i == n || k == (2 * i - 1)) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println("");
+        }
+    }
+    public static void main(String args[])
+    {
+        int n = 25;
+        printPattern(n);
+    }
+}
